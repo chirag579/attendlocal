583be1c0f15145681c59e3cfcea3f5ea1c244e07
zenithbale <zenithbale9@gmail.com>
2025-10-08 11:55:13 +0530

    final

--- serv1/index.js
+++ serv1/index.js
@@ -15,7 +15,10 @@ dotenv.config();
 
 connectToDatabase()
 const app=express()
-app.use(cors(
+app.use(cors({
+  origin:"https://attendlocalfront.vercel.app",
+  credentials:true
+}
 ))
 app.use(express.json())
 app.use(express.static('public/uploads'))
